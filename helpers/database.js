f49b82ec77293d583e7bccceef62ac8754fe945e
Mike Goodwin <mike.goodwin@owasp.org>
2017-02-09 21:44:00 +0000

    basic wire up to vault

--- helpers/database.js
+++ helpers/database.js
@@ -2,19 +2,76 @@
 
 var database = {};
 var mysql = require('mysql');
+var request = require('request');
+var logger = require('../config/loggers').logger;
+var vaultCredAddr = process.env.VAULT_CRED_ADDR;
+var vaultLeaseAddr = process.env.VAULT_LEASE_ADDR;
+var preLease = process.env.VAULT_PRE_LEASE;
+var token = process.env.VAULT_TOKEN;
+var connectionPool;
 var connectionInfo = {
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
 };
 
-var pool = mysql.createPool(connectionInfo);
- 
+function recyclePool() {
+
+}
+
+function refreshPool() {
+
+}
+
+function createPool() {
+
+  getCreds(function (creds, error) {
+
+    if (error) {
+
+      logger.error(error);
+
+    } else {
+
+      connectionInfo.user = creds.data.username;
+      connectionInfo.password = creds.data.password;
+      pool = mysql.createPool(connectionInfo);
+
+    }
+  });
+}
+
+function getCreds(cb) {
+
+  var options = {
+    url: vaultCredAddr,
+    headers: {
+      'X-Vault-Token': token
+    }
+  };
+
+  request(options, function (error, response, body) {
+
+    if (error || response != 200) {
+
+      cb(JSON.parse(body));
+
+    } else {
+
+      cb(null, error);
+    }
+
+  });
+}
+
+function renewLease() {
+
+}
+
 function getConnection(cb) {
-    pool.getConnection(cb);
+  pool.getConnection(cb);
 };
 
+createPool();
 database.getConnection = getConnection;
 
 module.exports = database;
--- repositories/world.js
+++ repositories/world.js
@@ -3,15 +3,17 @@
 var worldRepository = {};
 var database = require('../helpers/database');
 
-
 worldRepository.getCapital = function (country, cb) {
-
     database.getConnection(function (error, connection) {
+
         if (error) {
             cb(null, 'error connecting to database: ' + error.stack);
         }
 
         connection.query('select country.name as country, city.name as capital from city join country on city.ID = country.Capital where country.name = ? limit 1', [country], function (error, results, fields) {
+            
+            connection.release();
+
             if (error) {
                 cb(null, error);
             }
