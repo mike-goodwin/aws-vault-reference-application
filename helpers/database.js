720bd059b04161fcc9a6e9c52bf44650121e7149
Mike Goodwin <mike.goodwin@owasp.org>
2017-03-14 23:15:35 +0000

    basic lease management

--- helpers/database.js
+++ helpers/database.js
@@ -8,19 +8,11 @@ var vaultCredAddr = process.env.VAULT_CRED_ADDR;
 var vaultLeaseAddr = process.env.VAULT_LEASE_ADDR;
 var preLease = process.env.VAULT_PRE_LEASE;
 var token = process.env.VAULT_TOKEN;
-var connectionPool;
+var pool;
 var connectionInfo = {
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE
 };
-
-function recyclePool() {
-
-}
-
-function refreshPool() {
-
-}
 
 function createPool() {
 
@@ -34,13 +26,22 @@ function createPool() {
 
       connectionInfo.user = creds.data.username;
       connectionInfo.password = creds.data.password;
-      pool = mysql.createPool(connectionInfo);
 
+      if (pool) {
+        pool.end();
+      }
+
+      pool = mysql.createPool(connectionInfo);
+      logger.info('using db user: ' + creds.data.username + "with lease: " + creds.lease_id);
+      logger.info('can renew credentials: ' + creds.renewable);
+      renewOrRefresh(creds);
     }
   });
 }
 
 function getCreds(cb) {
+
+  logger.info('fetching credentials');
 
   var options = {
     url: vaultCredAddr,
@@ -51,20 +52,55 @@ function getCreds(cb) {
 
   request(options, function (error, response, body) {
 
-    if (error || response != 200) {
+    if (error || response.statusCode != 200) {
 
-      cb(JSON.parse(body));
+      cb(null, error);
 
     } else {
 
-      cb(null, error);
+      cb(JSON.parse(body));
     }
 
   });
 }
 
-function renewLease() {
+function renewLease(creds) {
 
+  logger.info('renewing lease');
+
+  var options = {
+    method: 'PUT',
+    url: vaultLeaseAddr + creds.lease_id,
+    headers: {
+      'X-Vault-Token': token
+    }
+  };
+
+  request(options, function (error, response, body) {
+
+    if (error || response.statusCode != 200) {
+
+      //todo: handle error
+
+    } else {
+      var newCreds = JSON.parse(body);
+      logger.info('renewed creds. can renew again:' + newCreds.renewable);
+      logger.info('new lease time: ' + newCreds.lease_duration);
+      renewOrRefresh(newCreds);
+    }
+
+  });
+}
+
+function renewOrRefresh(creds) {
+
+  var timeout = 1000 * (creds.lease_duration - preLease);
+
+  if (creds.renewable) {
+    setTimeout(renewLease, timeout, creds);
+  } else {
+    setTimeout(createPool, timeout);
+  }
 }
 
 function getConnection(cb) {
